3b6557b5aed01c19bb3af9582abcd0c4d23f3486
marcin86junior <marcin86.design@gmail.com>
2022-10-24 20:09:41 +0200

    add docker-test-cover to docker-entrypoint (working) / version 1.0

--- mysite/docker-entrypoint.sh
+++ mysite/docker-entrypoint.sh
@@ -6,6 +6,11 @@ python manage.py makemigrations
 python manage.py migrate
 python manage.py migrate --run-syncdb
 
+echo "Test website"
+python manage.py test
+coverage run --source='.' --omit='*migrations*,*init*,*wsgi*,*asgi*,*urls*,*manage*,*admin*,*apps*,*settings*,*test*,*seriali*' manage.py test
+coverage report
+
 # Start server
 echo "Starting server"
 python manage.py runserver 0.0.0.0:8000
--- mysite/urlcut/tests.py
+++ mysite/urlcut/tests.py
@@ -4,9 +4,14 @@ from urlcut.models import Link
 
 class ModelsTestCase(TestCase):
 
-    def test_Package_model(self):
+    def test_Link_model(self):
         """Link model work properly"""
         link = Link.objects.create(original_link='https://www.google.pl/',)
+        self.assertEqual(link.original_link, 'https://www.google.pl/')
+
+    def test_Link_model_premium(self):
+        """Link model work properly for premium-custom link"""
+        link = Link.objects.create(original_link='https://www.google.pl/', shortened_link='http://127.0.0.1:8000/abc/')
         self.assertEqual(link.original_link, 'https://www.google.pl/')
 
 class ViewsTestCase(TestCase):
@@ -17,7 +22,7 @@ class ViewsTestCase(TestCase):
         resp = self.client.get(link.shortened_link)
         self.assertEqual(resp.status_code, 301)
 
-    def test_main_page(self):
+    def test_homepage(self):
         """The main page loads properly"""
         response = self.client.get('http://127.0.0.1:8000/')
         self.assertEqual(response.status_code, 200)
@@ -28,44 +33,32 @@ class ViewsTestCase(TestCase):
         response = self.client.get('http://127.0.0.1:8000/list/')
         self.assertEqual(response.status_code, 200)
 
+    def remove1_page(self):
+        """Remove link page loads properly"""
+        response = self.client.get('http://127.0.0.1:8000/remove_unused/zerocount/')
+        self.assertEqual(response.status_code, 200)
+
+    def remove2_page(self):
+        """Remove link page loads properly"""
+        response = self.client.get('http://127.0.0.1:8000/remove_unused/minute/')
+        self.assertEqual(response.status_code, 200)
+
+    def remove3_page(self):
+        """Remove link page loads properly"""
+        response = self.client.get('http://127.0.0.1:8000/remove_used/minute/')
+        self.assertEqual(response.status_code, 200)
+
+    def remove4_page(self):
+        """Remove link page loads properly"""
+        response = self.client.get('http://127.0.0.1:8000/remove_used/day/')
+        self.assertEqual(response.status_code, 200)
+
+    def remove5_page(self):
+        """Remove link page loads properly"""
+        response = self.client.get('http://127.0.0.1:8000/remove_unused/all/')
+        self.assertEqual(response.status_code, 200)
+
     def test_admin_link_turned_off(self):
         """The admin should be turned-off"""
         resp = self.client.get('http://127.0.0.1:8000/admin/')
         self.assertNotEqual(resp.status_code, 404)
-
-'''
-# my first celery test
-class CeleryTasksTestCase(TestCase):
- 
-    def test_add_task(self):
-        from mysite.tasks import sample_task
-      
-        response = sample_task.apply(args=(4, 4)).get()
-        self.assertEqual(response, 8)
-'''
-
-'''
-# REST tests
-
-from django.urls import reverse
-from rest_framework import status
-from rest_framework.test import APITestCase
-
-class ReadUserTest(APITestCase):
-    def setUp(self):
-        self.package = Package.objects.create(author='Test author', title="Test title")
-
-    def test_can_read_user_list(self):
-        response = self.client.get(reverse('package/'))
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    def test_can_read_user_detail(self):
-        response = self.client.get(reverse('package-detail', args=[self.package.id]))
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-'''
-
-'''
-Test that should be done:
-- searching data with fixtures...
-- restAPI - search / APIfactories...
-'''
